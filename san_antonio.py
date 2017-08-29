bae35cd11afdf22adf035b38c2c88bf106e11240
Debian Live user <amnesia@localhost>
2017-08-29 12:48:08 +0000

    ajout des quotes et characters

--- san_antonio.py
+++ san_antonio.py
@@ -1,1 +1,13 @@
-print("yo")
+quotes = [ 
+        "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+        "On doit pouvoir choisir entre s'écouter parler et se faire entendre." 
+]
+characters = [
+        "alvin et les chipmunks",
+        "Babar",
+        "Betty Boop",
+        "Calimero",
+        "Casper",
+        "le chat potté",
+        "Kirikou",
+]
