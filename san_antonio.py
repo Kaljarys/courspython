a842c093fbee730b2d1212b20b31ef188bc35bd3
Debian Live user <amnesia@localhost>
2017-08-29 15:58:45 +0000

    script pour afficher une quotes

--- san_antonio.py
+++ san_antonio.py
@@ -11,11 +11,18 @@ characters = [
         "le chat potté",
         "Kirikou",
 ]
+user_name = input("Tapez entrée pour connaitre une autre citation ou B pour quitter le programme")
 
-  #show random quote
 
-  #If user_answer is "B"
-   #leave the program
+def show_random_item(my_list):
+  #TODO: get a random number
+ item = my_list[0] #get an item from the list, just the first one for now
+ print(item) #TODO: show the quote in the interpreter
+ return "program is over" #returned value
 
- #Else:
+print(show_random_item(quotes))
+
+for character in characters:
+    character.capitalize()
+
   #show another quote
